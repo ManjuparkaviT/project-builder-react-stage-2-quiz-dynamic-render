0961b56c164d9331c0eaf8be6dced6b6ac0e384d
MANJUPARKAVI T <manjutv1999@gmail.com>
2021-02-22 23:56:13 +0530

    Dynamic Progression of quiz section is updated

--- quiz/src/component/QuizComponent.jsx
+++ quiz/src/component/QuizComponent.jsx
@@ -1,30 +1,55 @@
 import React, { Component } from 'react'
 import './Style.css';
+import Questions from './Questions.json';
 
 export default class QuizComponent extends Component {
+    constructor(){
+        super();
+        this.state={
+            value:1,
+            quiz:Questions[0]
+        }
+        this.handleNext=this.handleNext.bind(this);
+        this.handlePrevious=this.handlePrevious.bind(this);
+    }
+
+    handleNext = () =>{
+        this.setState({
+            value:this.state.value+1,
+            quiz:Questions[this.state.value]
+        })
+    }
+
+    handlePrevious = () =>{
+        this.setState({
+            value:this.state.value-1,
+            quiz:Questions[this.state.value]
+        })
+    }
+
     render() {
         return (
             <div className="main">
                 <div className="quiz">
                     <h1>Question</h1>
                     <div className="question">
-                        <h4>4 of 15</h4>
-                        <h3>Which is the only mammal that can't jump?</h3>
+                        <h4>{this.state.value} of 15</h4>
+                        <h3>{this.state.quiz.question}</h3>
                     </div>
                     <div className="options">
-                        <button className="btn1">Dog</button>
-                        <button className="btn2">Goat</button>
+                        <button className="btn1">{this.state.quiz.optionA}</button>
+                        <button className="btn2">{this.state.quiz.optionB}</button>
                         <br/>
                     </div>
                     <div className="options">
-                        <button className="btn3 ">Elephant</button>
-                        <button className="btn4">Lion</button>
+                        <button className="btn3 ">{this.state.quiz.optionC}</button>
+                        <button className="btn4">{this.state.quiz.optionD}</button>
                         <br></br>
                     </div>
                     <br/>
                     <div className="buttons">
-                        <button className="btn5">Previous</button>
-                        <button className="btn6">Next</button>
+                        <button className="btn5" onClick={this.handlePrevious}>Previous</button>
+                        <button className="btn6" onClick={this.handleNext}>Next</button>
                         <button className="btn7">Quit</button>  
                     </div>
                 </div>
